16f6a942110c91868eccd73be3b10dcb8cdd2681
Marko326 <1290246164@qq.com>
2026-07-30 00:59:05 +0800

    缩短游戏保存时间
    删除Now saving...文本并加快存档速度

--- engine/save.asm
+++ engine/save.asm
@@ -161,14 +161,6 @@ SaveSAV:
 	ret nz
 .save
 	call SaveSAVtoSRAM
-	coord hl, 1, 13
-	lb bc, 4, 18
-	call ClearScreenArea
-	coord hl, 1, 14
-	ld de,NowSavingString
-	call PlaceString
-	ld c,120
-	call DelayFrames
 	ld hl,GameSavedText
 	call PrintText
 	ld a, SFX_SAVE
@@ -176,9 +168,6 @@ SaveSAV:
 	call WaitForSoundToFinish
 	ld c,30
 	jp DelayFrames
-
-NowSavingString:
-	db "Now saving...@"
 
 SaveSAVConfirm:
 	call PrintText
