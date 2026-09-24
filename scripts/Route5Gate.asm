539ca1a8eeba4d0caf57b933a24faec09c16d889
MKJ <204941685+Marko326@users.noreply.github.com>
2026-09-24 23:57:57 +0800

    refactor(saffron): 合并饮料关卡逻辑并修正推回状态
    - 合并四个 Gate 的重复拦截逻辑，保留各自坐标、方向和 Route 7 特例
    - 自动拦截与手动对话统一使用当前 Gate 参数
    - 修正 Route 6/7/8 手动对话误用 Route 5 推回状态
    
    Version: SAF-5.36.00

--- scripts/Route5Gate.asm
+++ scripts/Route5Gate.asm
@@ -1,43 +1,58 @@
+Route8GateScript:
+Route7GateScript:
+Route6GateScript:
 Route5GateScript:
 	call EnableAutoTextBoxDrawing
-	ld a, [wRoute5GateCurScript]
-	ld hl, Route5GateScriptPointers
-	jp CallFunctionInTable
+	call SaffronGateGetData
+	ld a, [hli]
+	ld e, a
+	ld a, [hli]
+	ld d, a
+	ld a, [de]
+	and a
+	jr nz, SaffronGateWaitForPushback
 
-Route5GateScriptPointers:
-	dw Route5GateScript0
-	dw Route5GateScript1
-
-Route5GateScript_1df43:
-	ld a, D_UP
-	ld [wSimulatedJoypadStatesEnd], a
-	ld a, $1
-	ld [wSimulatedJoypadStatesIndex], a
-	jp StartSimulatingJoypadStates
-
-Route5GateScript0:
 	ld a, [wd728]
 	bit 6, a
 	ret nz
-	ld hl, CoordsData_1df8f
+	inc hl
+	ld a, [hli]
+	ld c, a
+	ld a, [hli]
+	ld b, a
+	push hl
+	ld h, b
+	ld l, c
 	call ArePlayerCoordsInArray
+	pop hl
 	ret nc
-	ld a, PLAYER_DIR_LEFT
+	ld a, [hli]
 	ld [wPlayerMovingDirection], a
 	xor a
 	ld [hJoyHeld], a
+	push de
+	push hl
 	callba RemoveGuardDrink
+	pop hl
+	pop de
 	ld a, [$ffdb]
 	and a
-	jr nz, .asm_1df82
+	jr nz, .gaveDrink
+
 	ld a, $2
 	ld [hSpriteIndexOrTextID], a
+	push hl
+	push de
 	call DisplayTextID
-	call Route5GateScript_1df43
+	pop de
+	pop hl
+	ld a, [hl]
+	call SaffronGateStartPushback
 	ld a, $1
-	ld [wRoute5GateCurScript], a
+	ld [de], a
 	ret
-.asm_1df82
+
+.gaveDrink
 	ld a, $3
 	ld [hSpriteIndexOrTextID], a
 	call DisplayTextID
@@ -45,20 +60,94 @@ Route5GateScript0:
 	set 6, [hl]
 	ret
 
-CoordsData_1df8f:
-	db 3,3
-	db 3,4
-	db $ff
-
-Route5GateScript1:
+SaffronGateWaitForPushback:
 	ld a, [wSimulatedJoypadStatesIndex]
 	and a
 	ret nz
+	push hl
+	push de
 	call Delay3
+	pop de
+	pop hl
 	xor a
 	ld [wJoyIgnore], a
-	ld [wRoute5GateCurScript], a
+	ld [de], a
+	bit 0, [hl]
+	ret z
+; Route 7's original script also cleared the generic map-script index here.
+	ld [wCurMapScript], a
 	ret
+
+SaffronGateStartPushback:
+; a = direction that sends the player back toward the route they entered from
+	ld [wSimulatedJoypadStatesEnd], a
+	ld a, $1
+	ld [wSimulatedJoypadStatesIndex], a
+	jp StartSimulatingJoypadStates
+
+SaffronGateGetData:
+; Keep map selection explicit so another gate can be added without depending on
+; map-ID spacing or changing the shared state machine.
+	ld a, [wCurMap]
+	cp ROUTE_6_GATE
+	jr z, .route6
+	cp ROUTE_7_GATE
+	jr z, .route7
+	cp ROUTE_8_GATE
+	jr z, .route8
+	ld hl, SaffronGateRoute5Data
+	ret
+.route6
+	ld hl, SaffronGateRoute6Data
+	ret
+.route7
+	ld hl, SaffronGateRoute7Data
+	ret
+.route8
+	ld hl, SaffronGateRoute8Data
+	ret
+
+; current-script pointer, flags, trigger coordinates, facing, pushback direction
+; flag bit 0 preserves Route 7's original wCurMapScript reset after movement.
+SaffronGateRoute5Data:
+	dw wRoute5GateCurScript
+	db 0
+	dw .Coords
+	db PLAYER_DIR_LEFT, D_UP
+.Coords
+	db 3, 3
+	db 3, 4
+	db $ff
+
+SaffronGateRoute6Data:
+	dw wRoute6GateCurScript
+	db 0
+	dw .Coords
+	db PLAYER_DIR_RIGHT, D_DOWN
+.Coords
+	db 2, 3
+	db 2, 4
+	db $ff
+
+SaffronGateRoute7Data:
+	dw wRoute7GateCurScript
+	db 1
+	dw .Coords
+	db PLAYER_DIR_UP, D_LEFT
+.Coords
+	db 3, 3
+	db 4, 3
+	db $ff
+
+SaffronGateRoute8Data:
+	dw wRoute8GateCurScript
+	db 0
+	dw .Coords
+	db PLAYER_DIR_LEFT, D_RIGHT
+.Coords
+	db 3, 2
+	db 4, 2
+	db $ff
 
 Route5GateTextPointers:
 	dw Route5GateText1
@@ -72,24 +161,42 @@ Route5GateText1:
 	TX_ASM
 	ld a, [wd728]
 	bit 6, a
-	jr nz, .asm_88856
+	jr nz, .alreadyAllowed
+	call SaffronGateGetData
+	ld a, [hli]
+	ld e, a
+	ld a, [hli]
+	ld d, a
+	inc hl
+	inc hl
+	inc hl
+	inc hl
+	push hl
+	push de
 	callba RemoveGuardDrink
+	pop de
+	pop hl
 	ld a, [$ffdb]
 	and a
-	jr nz, .asm_768a2
+	jr nz, .gaveDrink
+	push hl
+	push de
 	ld hl, Route5GateText2
 	call PrintText
-	call Route5GateScript_1df43
+	pop de
+	pop hl
+	ld a, [hl]
+	call SaffronGateStartPushback
 	ld a, $1
-	ld [wRoute5GateCurScript], a
+	ld [de], a
 	jp TextScriptEnd
-.asm_768a2
+.gaveDrink
 	ld hl, Route5GateText3
 	call PrintText
 	ld hl, wd728
 	set 6, [hl]
 	jp TextScriptEnd
-.asm_88856
+.alreadyAllowed
 	ld hl, SaffronGateText_1dff6
 	call PrintText
 	jp TextScriptEnd
@@ -113,5 +220,3 @@ Route5GateText3:
 SaffronGateText_1dff6:
 	TX_FAR _SaffronGateText_1dff6
 	db "@"
-
-
